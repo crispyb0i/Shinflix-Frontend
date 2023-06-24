82d6cc74559c44978074853443aa6d4500aa307c
David Shin <xdavidshinx@gmail.com>
2023-06-23 18:29:00 -0700

    add date picker to journal page

--- src/components/pages/journal/JournalPage.js
+++ src/components/pages/journal/JournalPage.js
@@ -1,6 +1,37 @@
+import { useState } from "react";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+
 export const JournalPage = () => {
+	const [watchDate, setWatchDate] = useState(null);
+
+	console.log(watchDate);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	const handleDateSelect = (date) => {
+		const currentDate = new Date().setHours(0, 0, 0, 0); // Set current date without time
+		const selectedDate = new Date(date).setHours(0, 0, 0, 0); // Set selected date without time
+
+		if (selectedDate > currentDate) {
+			// Date is in the future, don't update the state
+			return;
+		}
+
+		setWatchDate(date);
+	};
+
+	const isDayDisabled = (day) => {
+		return day > new Date();
+	};
+
 	return (
-		<form className={"flex min-h-screen items-center justify-center"}>
+		<form
+			className={"flex min-h-screen items-center justify-center"}
+			onSubmit={handleSubmit}
+		>
 			<div className="space-y-12">
 				<div className="border-b border-gray-900/10 pb-12">
 					<h2 className="text-base font-semibold leading-7 text-gray-900">
@@ -20,16 +51,12 @@ export const JournalPage = () => {
 							</label>
 							<div className="mt-2">
 								<div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-									{/* <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
-										workcation.com/
-									</span> */}
 									<input
 										type="text"
 										name="media_title"
 										id="media_title"
 										autoComplete="media_title"
 										className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-										// placeholder="Enter title"
 									/>
 								</div>
 							</div>
@@ -77,381 +104,27 @@ export const JournalPage = () => {
 									<option>⭐⭐⭐⭐⭐</option>
 								</select>
 							</div>
-							<div className="mt-2">
-								<div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-									{/* <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
-										workcation.com/
-									</span> */}
-									<input
-										datepicker="true"
-										type="text"
-										name="media_title"
-										id="media_title"
-										autoComplete="media_title"
-										className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-										// placeholder="Enter title"
-									/>
-								</div>
+							<div className="mt-20">
+								<h1 className="text-base font-semibold leading-7 text-gray-900">
+									Date watched:
+								</h1>
+								<DayPicker
+									mode="single"
+									selected={watchDate}
+									onSelect={handleDateSelect}
+									captionLayout="dropdown"
+									fromYear={2000}
+									toYear={new Date().getFullYear()}
+									showOutsideDays
+									disabled={isDayDisabled}
+									modifiersStyles={{
+										disabled: { color: "fff" },
+									}}
+								/>
 							</div>
 						</div>
-
-						{/* <div class="col-span-full">
-							<label
-								htmlFor="photo"
-								class="block text-sm font-medium leading-6 text-gray-900"
-							>
-								Photo
-							</label>
-							<div class="mt-2 flex items-center gap-x-3">
-								<svg
-									class="h-12 w-12 text-gray-300"
-									viewBox="0 0 24 24"
-									fill="currentColor"
-									aria-hidden="true"
-								>
-									<path
-										fillRule="evenodd"
-										d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-										clipRule="evenodd"
-									/>
-								</svg>
-								<button
-									type="button"
-									class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-								>
-									Change
-								</button>
-							</div>
-						</div> */}
-
-						{/* <div class="col-span-full">
-							<label
-								htmlFor="cover-photo"
-								class="block text-sm font-medium leading-6 text-gray-900"
-							>
-								Cover photo
-							</label>
-							<div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-								<div class="text-center">
-									<svg
-										class="mx-auto h-12 w-12 text-gray-300"
-										viewBox="0 0 24 24"
-										fill="currentColor"
-										aria-hidden="true"
-									>
-										<path
-											fillRule="evenodd"
-											d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
-											clipRule="evenodd"
-										/>
-									</svg>
-									<div class="mt-4 flex text-sm leading-6 text-gray-600">
-										<label
-											htmlFor="file-upload"
-											class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-										>
-											<span>Upload a file</span>
-											<input
-												id="file-upload"
-												name="file-upload"
-												type="file"
-												class="sr-only"
-											/>
-										</label>
-										<p class="pl-1">or drag and drop</p>
-									</div>
-									<p class="text-xs leading-5 text-gray-600">
-										PNG, JPG, GIF up to 10MB
-									</p>
-								</div>
-							</div>
-						</div> */}
 					</div>
 				</div>
-
-				{/* <div class="border-b border-gray-900/10 pb-12">
-					<h2 class="text-base font-semibold leading-7 text-gray-900">
-						Personal InhtmlFormation
-					</h2>
-					<p class="mt-1 text-sm leading-6 text-gray-600">
-						Use a permanent address where you can receive mail.
-					</p>
-
-					<div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-						<div class="sm:col-span-3">
-							<label
-								htmlFor="first-name"
-								class="block text-sm font-medium leading-6 text-gray-900"
-							>
-								First name
-							</label>
-							<div class="mt-2">
-								<input
-									type="text"
-									name="first-name"
-									id="first-name"
-									autoComplete="given-name"
-									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
-						</div>
-
-						<div class="sm:col-span-3">
-							<label
-								htmlFor="last-name"
-								class="block text-sm font-medium leading-6 text-gray-900"
-							>
-								Last name
-							</label>
-							<div class="mt-2">
-								<input
-									type="text"
-									name="last-name"
-									id="last-name"
-									autoComplete="family-name"
-									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
-						</div>
-
-						<div class="sm:col-span-4">
-							<label
-								htmlFor="email"
-								class="block text-sm font-medium leading-6 text-gray-900"
-							>
-								Email address
-							</label>
-							<div class="mt-2">
-								<input
-									id="email"
-									name="email"
-									type="email"
-									autoComplete="email"
-									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
-						</div>
-
-						<div class="sm:col-span-3">
-							<label
-								htmlFor="country"
-								class="block text-sm font-medium leading-6 text-gray-900"
-							>
-								Country
-							</label>
-							<div class="mt-2">
-								<select
-									id="country"
-									name="country"
-									autoComplete="country-name"
-									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-								>
-									<option>United States</option>
-									<option>Canada</option>
-									<option>Mexico</option>
-								</select>
-							</div>
-						</div>
-
-						<div class="col-span-full">
-							<label
-								htmlFor="street-address"
-								class="block text-sm font-medium leading-6 text-gray-900"
-							>
-								Street address
-							</label>
-							<div class="mt-2">
-								<input
-									type="text"
-									name="street-address"
-									id="street-address"
-									autoComplete="street-address"
-									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
-						</div>
-
-						<div class="sm:col-span-2 sm:col-start-1">
-							<label
-								htmlFor="city"
-								class="block text-sm font-medium leading-6 text-gray-900"
-							>
-								City
-							</label>
-							<div class="mt-2">
-								<input
-									type="text"
-									name="city"
-									id="city"
-									autoComplete="address-level2"
-									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
-						</div>
-
-						<div class="sm:col-span-2">
-							<label
-								htmlFor="region"
-								class="block text-sm font-medium leading-6 text-gray-900"
-							>
-								State / Province
-							</label>
-							<div class="mt-2">
-								<input
-									type="text"
-									name="region"
-									id="region"
-									autoComplete="address-level1"
-									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
-						</div>
-
-						<div class="sm:col-span-2">
-							<label
-								htmlFor="postal-code"
-								class="block text-sm font-medium leading-6 text-gray-900"
-							>
-								ZIP / Postal code
-							</label>
-							<div class="mt-2">
-								<input
-									type="text"
-									name="postal-code"
-									id="postal-code"
-									autoComplete="postal-code"
-									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
-						</div>
-					</div>
-				</div> */}
-
-				{/* <div class="border-b border-gray-900/10 pb-12">
-					<h2 class="text-base font-semibold leading-7 text-gray-900">
-						Notifications
-					</h2>
-					<p class="mt-1 text-sm leading-6 text-gray-600">
-						We'll always let you know about important changes, but you pick what
-						else you want to hear about.
-					</p>
-
-					<div class="mt-10 space-y-10">
-						<fieldset>
-							<legend class="text-sm font-semibold leading-6 text-gray-900">
-								By Email
-							</legend>
-							<div class="mt-6 space-y-6">
-								<div class="relative flex gap-x-3">
-									<div class="flex h-6 items-center">
-										<input
-											id="comments"
-											name="comments"
-											type="checkbox"
-											class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-										/>
-									</div>
-									<div class="text-sm leading-6">
-										<label htmlFor="comments" class="font-medium text-gray-900">
-											Comments
-										</label>
-										<p class="text-gray-500">
-											Get notified when someones posts a comment on a posting.
-										</p>
-									</div>
-								</div>
-								<div class="relative flex gap-x-3">
-									<div class="flex h-6 items-center">
-										<input
-											id="candidates"
-											name="candidates"
-											type="checkbox"
-											class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-										/>
-									</div>
-									<div class="text-sm leading-6">
-										<label htmlFor="candidates" class="font-medium text-gray-900">
-											Candidates
-										</label>
-										<p class="text-gray-500">
-											Get notified when a candidate applies htmlFor a job.
-										</p>
-									</div>
-								</div>
-								<div class="relative flex gap-x-3">
-									<div class="flex h-6 items-center">
-										<input
-											id="offers"
-											name="offers"
-											type="checkbox"
-											class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-										/>
-									</div>
-									<div class="text-sm leading-6">
-										<label htmlFor="offers" class="font-medium text-gray-900">
-											Offers
-										</label>
-										<p class="text-gray-500">
-											Get notified when a candidate accepts or rejects an offer.
-										</p>
-									</div>
-								</div>
-							</div>
-						</fieldset>
-						<fieldset>
-							<legend class="text-sm font-semibold leading-6 text-gray-900">
-								Push Notifications
-							</legend>
-							<p class="mt-1 text-sm leading-6 text-gray-600">
-								These are delivered via SMS to your mobile phone.
-							</p>
-							<div class="mt-6 space-y-6">
-								<div class="flex items-center gap-x-3">
-									<input
-										id="push-everything"
-										name="push-notifications"
-										type="radio"
-										class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-									/>
-									<label
-										htmlFor="push-everything"
-										class="block text-sm font-medium leading-6 text-gray-900"
-									>
-										Everything
-									</label>
-								</div>
-								<div class="flex items-center gap-x-3">
-									<input
-										id="push-email"
-										name="push-notifications"
-										type="radio"
-										class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-									/>
-									<label
-										htmlFor="push-email"
-										class="block text-sm font-medium leading-6 text-gray-900"
-									>
-										Same as email
-									</label>
-								</div>
-								<div class="flex items-center gap-x-3">
-									<input
-										id="push-nothing"
-										name="push-notifications"
-										type="radio"
-										class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-									/>
-									<label
-										htmlFor="push-nothing"
-										class="block text-sm font-medium leading-6 text-gray-900"
-									>
-										No push notifications
-									</label>
-								</div>
-							</div>
-						</fieldset>
-					</div>
-				</div> */}
 				<div className="mt-6 flex items-center justify-end gap-x-6">
 					<button
 						type="button"
@@ -470,3 +143,17 @@ export const JournalPage = () => {
 		</form>
 	);
 };
+
+// Format dates like this when adding to Firebase
+// const date = new Date(); // The date you want to store
+// const timestamp = firebase.firestore.Timestamp.fromDate(date);
+
+// Timestamp(seconds, nanoseconds)
+// Timestamp(1656748920, 123000000)
+
+// The seconds part represents the number of seconds since the Unix epoch
+// (January 1, 1970, 00:00:00 UTC), and the nanoseconds part represents
+// the additional fractional seconds.
+
+// Please note that the actual values in the console log may vary
+// depending on the specific timestamp and your timezone.
